304704faf8f2ceeac70ee7be3fa0ed470cd4b362
jkim <jihyo.kim@move.com>
2019-09-07 19:13:07 -0700

    modifed webpack config for routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+let path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
     module: {
         rules: [
             {
@@ -23,12 +29,15 @@ module.exports = {
                 test:/\.css$/,
                 use:['style-loader','css-loader']
             }
-        ]
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/index.html",
-            filename: "./index.html"
+            filename: "index.html"
         })
     ]
 };
